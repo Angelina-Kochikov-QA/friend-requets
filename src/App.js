4e997d1bdd7b8b8a782e91cb361ca5c05ddf48ce
“Angelina <mrs.angelinach@gmail.com>
2024-01-06 23:44:44 -0800

    changes to src/App.css, src/App.js, data.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [friends, setFriends] = useState(data);
+  
+
+const removeFriend = (id) => {
+  let newFriends = friends.filter((item) => item.id !== id);
+    setFriends(newFriends);
+}
+
+  return (<div>
+
+      <div className="container">
+    <h1>You have {friends.length} New friend requests</h1>
     </div>
-  );
+  
+  {friends.map((item => {
+    const {id, friendName, message, image} = item;
+
+
+    return (<div key={id}>
+
+      <div className="container">
+        <h2>{id}-{friendName}</h2>
+      </div>
+
+      <div className="container">
+        <p>Message: "{message}"</p>
+      </div>
+
+      <div className="container">
+        <img src={image} width="300px" alt="friend"/>
+      </div>
+
+
+      <div className="container">
+        <button className="btn" onClick={() => removeFriend(id)}>Remove request</button>
+      </div>
+
+      </div>)
+
+      
+  }))}
+  <div className='container'>
+  <button className='btn' onClick={() => setFriends([])}>Delete all requests</button>
+  </div>
+  </div>
+  
+
+)
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const data = [
+
+    {
+    
+        id: 1,
+        friendName: "Sara Washington",
+        message: "Hi there👋🏻. Do you remember me?",
+        image: "https://images.unsplash.com/photo-1521567097888-2c5fc40a8660?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        
+    
+    },
+    
+    {
+    
+        id: 2,
+        friendName: "Tony Maxwell",
+        message: "Let's chat!😊",
+        image: "https://images.unsplash.com/photo-1522556189639-b150ed9c4330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    
+    
+    },
+    
+    {
+    
+        id: 3,
+        friendName: "Ella Parker",
+        message: "Hey, it's Ella! I'm finally found you here🥰",
+        image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        
+    },
+
+    {
+    
+        id: 4,
+        friendName: "James Brown",
+        message: "I came across your profile and want to connect☺️",
+        image: "https://images.unsplash.com/photo-1594672830234-ba4cfe1202dc?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        
+    
+    },
+
+    {
+    
+        id: 5,
+        friendName: "Jessica Spenser",
+        message: "Hi, what's up?😜",
+        image: "https://images.unsplash.com/photo-1686317278178-ee96724bdec9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        
+    
+    }
+    
+    ]
+    
+    
+    
+    
+    
+    
+    
+    
+    
